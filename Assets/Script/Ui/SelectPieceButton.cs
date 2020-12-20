496e396bdfac2bafebd248e290ed1bce23a2f9f3
nckuwater <f74081195@gs.ncku.edu.tw>
2020-12-21 01:05:38 +0800

    Building ui

---
+++ Assets/Script/Ui/SelectPieceButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets.Script;
+
+public class SelectPieceButton : MonoBehaviour {
+    public delegate void PieceButtonClickedDelegate();
+    public PieceButtonClickedDelegate PieceButtonClickedEvent;
+    public Piece.PieceType type;
+    public Button btn;
+	// Use this for initialization
+	void Start () {
+        btn = GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
